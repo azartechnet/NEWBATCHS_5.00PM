f6fa41bcaf81275e4237558e01afe68e7f948ad4
azartechnet <azartechnet@gmail.com>
2026-09-09 18:11:11 +0530

    updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import StudentForm from "./components/StudentForm";
+
+function App()
+{
+  return(
+    <div className="container mt-5">
+        <h2>StudentCRUD Application</h2>
+        <h4>React Front Connected!!!</h4>
+        <StudentForm/>
+    </div>
+  )
+}
+export default App;
